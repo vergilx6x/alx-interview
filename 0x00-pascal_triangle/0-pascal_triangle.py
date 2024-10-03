216b203cbc0d6489d51719e0a7a986b6b01a9df5
vergilx6x <med.work96@hotmail.com>
2024-10-03 17:25:43 +0100

    Done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+import math
+
+
+def pascal_triangle(n):
+    list_of_rows = []
+
+    if n <= 0:
+        return list_of_rows
+
+    for row in range(n):
+        columns = []
+        for col in range(row + 1):
+
+            if col == 0 or col == row:
+                columns.append("1")
+            else:
+                columns.append(math.comb(row, col))
+
+        list_of_rows.append(columns)
+
+    return list_of_rows
