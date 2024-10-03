66ba2939736a46e0ded2c0dd22862ecbc7cac39f
vergilx6x <med.work96@hotmail.com>
2024-10-03 17:29:13 +0100

    Done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,16 +5,16 @@ def pascal_triangle(n):
     list_of_rows = []
 
     if n <= 0:
-        return list_of_rows
+        return []
 
     for row in range(n):
         columns = []
         for col in range(row + 1):
 
             if col == 0 or col == row:
-                columns.append("1")
+                columns.append(1)
             else:
-                columns.append(math.comb(row, col))
+                columns.append((int(math.comb(row, col))))
 
         list_of_rows.append(columns)
 
