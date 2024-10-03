23aaf1e896e3387b48c825c2992639eaaf662aa6
vergilx6x <med.work96@hotmail.com>
2024-10-03 17:37:53 +0100

    Done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,10 +3,11 @@ import math
 
 ''' A module for working with Pascal's triangle. '''
 
+
 def pascal_triangle(n):
     '''Creates a list of lists of integers representing
     the Pascal's triangle of a given integer.'''
-    
+
     list_of_rows = []
 
     if type(n) is not int or n <= 0:
