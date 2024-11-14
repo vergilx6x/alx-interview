7704d42a47f54a4d22dd3ea63759ff0aa85ed9f6
vergilx6x <med.work96@hotmail.com>
2024-11-15 00:20:46 +0100

    Done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,22 +2,24 @@
 const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv[2]) {
+if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) return console.error(err);
-
-    const characters = JSON.parse(body).characters;
-    const characterPromises = characters.map(url =>
-      new Promise((resolve, reject) => {
-        request(url, (error, __, charBody) => {
-          if (error) return reject(error);
-          resolve(JSON.parse(charBody).name);
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
         });
-      })
-    );
+      }));
 
-    Promise.all(characterPromises)
+    Promise.all(charactersName)
       .then(names => console.log(names.join('\n')))
-      .catch(console.error);
+      .catch(allErr => console.log(allErr));
   });
 }
