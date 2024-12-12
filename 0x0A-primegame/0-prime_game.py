4798efcd009601cbd9e2661088de2733ea38a76e
vergilx6x <med.work96@hotmail.com>
2024-12-13 00:44:41 +0100

    Challenge done

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Prime game module.
+"""
+
+
+def isWinner(x, nums):
+    """Determines the winner of a prime game session with `x` rounds."""
+    if x < 1 or not nums:
+        return None
+
+    max_num = max(nums)
+    primes = [True] * (max_num + 1)
+    primes[0] = primes[1] = False
+
+    for i in range(2, int(max_num**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_num + 1, i):
+                primes[j] = False
+
+    prime_counts = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
+
+    maria_wins, ben_wins = 0, 0
+
+    for n in nums:
+        if prime_counts[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    return None
